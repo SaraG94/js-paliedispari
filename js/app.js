f61c9bdb70b17969b951410e32ecf565b7847fec
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-08 15:51:53 +0100

    START

---
+++ js/app.js
@@ -0,0 +1,20 @@
+console.log('ciao');
+
+//palindromia
+//chiedere all'utente una parola
+//capire se la parola è palindroma
+  //scomporre la parola
+  //verificare se la prima lettera è uguale all'ultima
+  //verifcare le altre lettere(ciclo)
+//comunicare all'utente se è palindroma o non lo è
+
+
+//pari e dispari
+//utente inserisce un numero da 1 a 5
+// pc genera un numero random da 1 a 5
+//somma i numeri
+//se somma pari 
+    //utente vince
+//altrimenti
+    //pc vince
+//comunicare risultati
